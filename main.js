fc3d3b801294c7b1c083ba3fdc9e69cdc46397e6
sntsmaju <166028669+sntsmaju@users.noreply.github.com>
2024-09-19 14:02:00 +0000

    arquivo js

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".abas-conteudo");
+
+for(let i =0; i < botoes.length; i++){
+    botoes[i].onclick = function (){
+
+        for(let j = 0)
+    }
+}
